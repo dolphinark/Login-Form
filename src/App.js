50e45b4970b9427383bdca18a9db3dce3320fbb7
Rita <ritahung19@gmail.com>
2022-07-22 20:24:11 +0800

    login and logout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import LoginForm from './components/LoginForm';
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  const userName = [{
-    email:"123@yahoo.com.tw",
-    password:"123"
-  }]
+  const [login, setLogin] = useState(false);
+  const [useName, setUserName] = useState("")
+  const userName = {
+    email: "123@yahoo.com.tw",
+    password: "123",
+  };
 
- 
+  function handleLogin(details) {
+    setUserName(details.name)
+  }
+
+  function handleLogout(){
+    setLogin(false)
+  }
+
   return (
     <div className="App">
-      <LoginForm userName={userName}/>
+      {login ? (
+        <>
+        <span>Welcome {useName}</span> <button onClick={handleLogout}>Log out</button>
+        </>
+      ) : (
+        <LoginForm setLogin={setLogin} handleLogin={handleLogin} userName={userName} />
+      )}
     </div>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,41 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 
-function LoginForm({userName}) {
-    const emailRef= useRef()
-    const passwordRef = useRef()
+function LoginForm({ userName, handleLogin, setLogin }) {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
+  function loginForm(e) {
+    e.preventDefault();
 
-    function login(e){
-        e.preventDefault()
-        if(emailRef.current===userName.email && passwordRef.current===userName.password){
-            console.log("login")
-        }
-        else{
-            console.log("incorrect")
-        }
+    const enteredName = nameRef.current.value;
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+
+    const details = {
+      name: enteredName,
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+    if (
+      enteredEmail === userName.email &&
+      enteredPassword === userName.password
+    ) {
+      setLogin(true);
+      handleLogin(details);
+    } else {
+      console.log("incorrect");
     }
-   
+  }
+
   return (
-    <form className="container" onClick={login}>
+    <form className="container" onClick={loginForm}>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" />
+      <input type="text" id="name" ref={nameRef} />
       <label htmlFor="email">E-mail</label>
-      <input type="email" id="email" ref={emailRef}/>
+      <input type="email" id="email" ref={emailRef} />
       <label htmlFor="password">Password</label>
-      <input type="text" id="password" ref={passwordRef}/>
+      <input type="text" id="password" ref={passwordRef} />
       <button>Login</button>
     </form>
   );
